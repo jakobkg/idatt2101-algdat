d296323463815d5f3527e9bd8c2deff530e68b21
Jakob Grønhaug <jakob@gronha.ug>
2022-11-07 15:03:02 +0100

    ✨ Huffman koding ferdig!

--- 08/src/main.rs
+++ 08/src/main.rs
@@ -1,3 +1,18 @@
+use std::{env::args, process::exit};
+
+#[macro_export]
+macro_rules! get_og_iterer {
+    ( $arr:ident, $idx:ident ) => {
+        match $arr.get($idx) {
+            Some(&byte) => {
+                $idx += 1;
+                byte
+            }
+            None => return Err("En uventet feil oppsto ved dekomprimering".to_string()),
+        }
+    };
+}
+
 mod fil {
     use std::{
         fs::File,
@@ -143,19 +158,6 @@ mod lz77 {
     }
 
     pub fn dekomprimer(data: &[u8]) -> Result<Vec<u8>, String> {
-        #[macro_export]
-        macro_rules! get_og_iterer {
-            ( $arr:ident, $idx:ident ) => {
-                match $arr.get($idx) {
-                    Some(&byte) => {
-                        $idx += 1;
-                        byte
-                    }
-                    None => return Err("En uventet feil oppsto ved dekomprimering".to_string()),
-                }
-            };
-        }
-
         let mut dekomprimert: Vec<u8> = Vec::new();
 
         let mut posisjon: usize = 0;
@@ -201,19 +203,18 @@ mod lz77 {
 
 mod huffman {
     use std::collections::BinaryHeap;
-    use typer::ByteFrekvens;
 
     use typer::Node;
 
-    use self::typer::Hopp;
+    use typer::{ByteFrekvens, Hopp};
 
     mod typer {
         use std::cmp::Ordering;
 
-        #[derive(Debug)]
+        #[derive(Debug, Clone, Copy)]
         pub struct ByteFrekvens {
-            byte: u8,
-            frekvens: u32,
+            pub byte: u8,
+            pub frekvens: u32,
         }
 
         impl ByteFrekvens {
@@ -224,6 +225,32 @@ mod huffman {
             pub fn øk(&mut self) -> () {
                 self.frekvens += 1;
             }
+
+            pub fn til_bytes(&self) -> [u8; 5] {
+                let mut resultat: [u8; 5] = [0, 0, 0, 0, 0];
+                let frekvensbytes = self.frekvens.to_be_bytes();
+
+                for i in 0..frekvensbytes.len() {
+                    resultat[i] = frekvensbytes[i];
+                }
+
+                resultat[4] = self.byte;
+
+                resultat
+            }
+
+            pub fn fra_bytes(bytes: [u8; 5]) -> Self {
+                let mut frekvensbytes: [u8; 4] = [0, 0, 0, 0];
+
+                for i in 0..4 {
+                    frekvensbytes[i] = bytes[i];
+                }
+
+                Self {
+                    byte: bytes[4],
+                    frekvens: u32::from_be_bytes(frekvensbytes),
+                }
+            }
         }
 
         #[derive(Clone, Copy, Debug)]
@@ -232,7 +259,7 @@ mod huffman {
             Høyre,
         }
 
-        #[derive(Debug)]
+        #[derive(Debug, Clone)]
         pub struct Node {
             pub frekvens: u32,
             pub verdi: Option<u8>,
@@ -263,8 +290,8 @@ mod huffman {
                     høyre: None,
                 }
             }
-        
-            pub fn finn_node(&self, verdi: u8, resultat: &mut Vec<Hopp>) -> bool {
+
+            pub fn finn_node(&self, verdi: u8, sti: &mut Vec<Hopp>) -> bool {
                 let mut res: bool = false;
 
                 if Some(verdi) == self.verdi {
@@ -273,28 +300,28 @@ mod huffman {
 
                 match &self.venstre {
                     Some(venstre) => {
-                        resultat.push(Hopp::Venstre);
+                        sti.push(Hopp::Venstre);
 
-                        if venstre.finn_node(verdi, resultat) {
+                        if venstre.finn_node(verdi, sti) {
                             res = true;
                         } else {
-                            resultat.pop();
+                            sti.pop();
                         }
-                    },
-                    None => {},
+                    }
+                    None => {}
                 };
 
                 match &self.høyre {
                     Some(høyre) => {
-                        resultat.push(Hopp::Høyre);
+                        sti.push(Hopp::Høyre);
 
-                        if høyre.finn_node(verdi, resultat) {
+                        if høyre.finn_node(verdi, sti) {
                             res = true;
                         } else {
-                            resultat.pop();
+                            sti.pop();
                         }
-                    },
-                    None => {},
+                    }
+                    None => {}
                 };
 
                 return res;
@@ -331,9 +358,177 @@ mod huffman {
     pub fn komprimer(data: &[u8]) -> Vec<u8> {
         let mut komprimert: Vec<u8> = Vec::new();
 
-        let frekvenser = finn_frekvenser(data);
-        let noder = lag_noder(&frekvenser);
+        let frekvenstabell = finn_frekvenser(data);
 
+        // Skriv lengden på frekvenstabellen til output
+        for byte in frekvenstabell.len().to_be_bytes() {
+            komprimert.push(byte);
+        }
+
+        // Skriv selve frekvenstabellen til output
+        for frekvens in frekvenstabell.iter() {
+            for byte in frekvens.til_bytes() {
+                komprimert.push(byte);
+            }
+        }
+
+        let tre = konstruer_tre(frekvenstabell);
+        let mut sti: Vec<Hopp> = Vec::new();
+        let mut totalsti: Vec<Hopp> = Vec::new();
+
+        // For hver byte i input-dataen, finn den korresponderende stien i Huffman-treet
+        for &byte in data {
+            if !tre.finn_node(byte, &mut sti) {
+                panic!("Verdi ikke i treet");
+            }
+
+            // Sett sammen en "totalsti" for all dataen i treet
+            totalsti.append(&mut sti);
+        }
+
+        // Skriv lengden på totalstien til output
+        for byte in totalsti.len().to_be_bytes() {
+            komprimert.push(byte);
+        }
+
+        let mut bytebuffer = 0b0000_0000u8;
+        let mut bit_teller = 0;
+
+        // Iterer gjennom den ovennevnte totalstien og bygg bytes til output av dem
+        for hopp in totalsti.iter() {
+            // Forskyv alle bits i byten som bygges en plass til venstre
+            bytebuffer = bytebuffer.rotate_left(1);
+
+            match hopp {
+                // Å gå til venstre i treet representeres ved bit 0
+                Hopp::Venstre => {}
+                // Å gå til høyre i treet representeres ved bit 1
+                Hopp::Høyre => {
+                    bytebuffer += 1;
+                }
+            };
+
+            bit_teller += 1;
+
+            // For hver åttende bit som er behandlet er en byte ferdig og klar for output
+            if bit_teller == 8 {
+                // Skriv ferdig byte
+                komprimert.push(bytebuffer);
+
+                // Reset teller og buffer
+                bit_teller = 0;
+                bytebuffer = 0;
+            }
+        }
+
+        // Om løkken ikke hadde flaks og stoppet på kanten av en byte
+        if bit_teller != 0 {
+            // Plasser bits korrekt i siste byte og legg den til i komprimert data
+            bytebuffer = bytebuffer.rotate_left(bit_teller);
+            komprimert.push(bytebuffer);
+        }
+
+        komprimert
+    }
+
+    pub fn dekomprimer(data: &[u8]) -> Result<Vec<u8>, String> {
+        let mut resultat: Vec<u8> = Vec::new();
+        let mut posisjon: usize = 0;
+
+        // Les lengden på frekvenstabellen fra starten av dataen
+        let mut tabell_lengde_bytes: [u8; 8] = [0, 0, 0, 0, 0, 0, 0, 0];
+
+        for i in 0..8 {
+            tabell_lengde_bytes[i] = get_og_iterer!(data, posisjon);
+        }
+
+        let tabell_lengde = usize::from_be_bytes(tabell_lengde_bytes);
+
+        // Les de neste 5*n bytes som frekvenstabell
+        let mut frekvenstabell: Vec<ByteFrekvens> = Vec::new();
+        let mut frekvens_bytes: [u8; 5] = [0, 0, 0, 0, 0];
+
+        for _ in 0..tabell_lengde {
+            for i in 0..5 {
+                frekvens_bytes[i] = get_og_iterer!(data, posisjon);
+            }
+
+            frekvenstabell.push(ByteFrekvens::fra_bytes(frekvens_bytes));
+        }
+
+        // Konstruer et Huffman-tre fra den elste frekvenstabellen
+        let rotnode = konstruer_tre(frekvenstabell);
+
+        // Les antallet bits den gjenstående dataen skal bestå av
+        let mut data_lengde_bytes: [u8; 8] = [0, 0, 0, 0, 0, 0, 0, 0];
+
+        for i in 0..8 {
+            data_lengde_bytes[i] = get_og_iterer!(data, posisjon);
+        }
+
+        let data_lengde = usize::from_be_bytes(data_lengde_bytes);
+
+        let mut bytebuffer: u8 = get_og_iterer!(data, posisjon);
+        let mut bit_teller = 0;
+        let mut node: Node = rotnode.clone();
+
+        while bit_teller < data_lengde {
+            if bytebuffer > 127 {
+                node = *node.høyre.unwrap();
+            } else {
+                node = *node.venstre.unwrap();
+            }
+
+            bit_teller += 1;
+            bytebuffer = bytebuffer.rotate_left(1);
+
+            if let Some(verdi) = node.verdi {
+                resultat.push(verdi);
+                node = rotnode.clone();
+            }
+
+            if bit_teller % 8 == 0 {
+                bytebuffer = get_og_iterer!(data, posisjon);
+            }
+        }
+
+        Ok(resultat)
+    }
+
+    fn finn_frekvenser(data: &[u8]) -> Vec<ByteFrekvens> {
+        let mut frekvenser: Vec<ByteFrekvens> = Vec::new();
+
+        let mut alle_frekvenser: Vec<ByteFrekvens> = Vec::new();
+
+        for byte in 0u8..u8::MAX {
+            alle_frekvenser.push(ByteFrekvens::ny(byte));
+        }
+
+        for &byte in data {
+            alle_frekvenser[byte as usize].øk();
+        }
+
+        for frekvens in alle_frekvenser {
+            if frekvens.frekvens > 0 {
+                frekvenser.push(frekvens);
+            }
+        }
+
+        frekvenser
+    }
+
+    fn lag_noder(frekvenser: &Vec<ByteFrekvens>) -> Vec<Node> {
+        let mut resultat: Vec<Node> = Vec::new();
+
+        for frekvens in frekvenser {
+            resultat.push(Node::fra_frekvens(frekvens));
+        }
+
+        resultat
+    }
+
+    fn konstruer_tre(frekvenstabell: Vec<ByteFrekvens>) -> Node {
+        let noder = lag_noder(&frekvenstabell);
         let mut kø = BinaryHeap::from(noder);
         let mut ny_node;
         let mut venstre;
@@ -355,49 +550,9 @@ mod huffman {
             kø.push(ny_node)
         }
 
-        let tre = kø.pop().unwrap(); // Trygg unwrap siden løkken over garanterer at køen har nøyaktig ett element igjen
-
-        let mut sti: Vec<Hopp> = Vec::new();
-        tre.finn_node(101u8, &mut sti);
-
-        println!("{sti:?}");
-        komprimert
-    }
-
-    fn finn_frekvenser(data: &[u8]) -> Vec<ByteFrekvens> {
-        let mut frekvenser: Vec<ByteFrekvens> = Vec::new();
-
-        let mut data = Vec::from(data);
-
-        data.sort();
-
-        let mut forrige_byte = data[0];
-        let mut frekvens = ByteFrekvens::ny(forrige_byte);
-
-        for byte in data {
-            if byte == forrige_byte {
-                frekvens.øk();
-            } else {
-                frekvenser.push(frekvens);
-                frekvens = ByteFrekvens::ny(byte);
-                frekvens.øk();
-                forrige_byte = byte;
-            }
-        }
-
-        frekvenser.push(frekvens);
-
-        frekvenser
-    }
-
-    fn lag_noder(frekvenser: &Vec<ByteFrekvens>) -> Vec<Node> {
-        let mut resultat: Vec<Node> = Vec::new();
-
-        for frekvens in frekvenser {
-            resultat.push(Node::fra_frekvens(frekvens));
-        }
-
-        resultat
+        // Følgende er en trygg unwrap siden løkken over garanterer at køen har nøyaktig ett element igjen
+        // Dette elementet er rot-noden i Huffman-treet
+        kø.pop().unwrap()
     }
 }
 
@@ -407,7 +562,7 @@ fn print_hjelp(args: Vec<String>) {
     println!("\t-k, --komprimer:   Komprimer den angitte filen");
     println!("\t-d, --dekomprimer: Dekomprimer den angitte filen");
     println!("\t-o, --output:      Valgfritt filnavn å skrive resultatet til.");
-    println!("\t\tDefault er å bruke samme filnavn som input, og legge til eller fjerne \".lz\"");
+    println!("\t\tDefault er å bruke samme filnavn som input, og legge til eller fjerne \".lzh\"");
 }
 
 enum Handling {
@@ -415,13 +570,18 @@ enum Handling {
     Dekomprimer,
 }
 
-fn main() {
-    let mut data = fil::les_bytes("testfile").unwrap();
-
-    huffman::komprimer(&data);
-}
-
 /*
+fn main() {
+    let data = fil::les_bytes("testfile").unwrap();
+
+    let komprimert = huffman::komprimer(&data);
+
+    let dekomprimert = huffman::dekomprimer(&komprimert);
+
+    fil::skriv_bytes("test.huff", &dekomprimert.unwrap());
+}
+*/
+
 fn main() {
     let args: Vec<String> = args().collect();
 
@@ -447,7 +607,7 @@ fn main() {
 
             match handling {
                 Handling::Dekomprimer => {
-                    if !input.ends_with(".lz") {
+                    if !input.ends_with(".lzh") {
                         println!(
                             "{} virker ikke å være komprimert med dette programmet",
                             args[2]
@@ -458,7 +618,7 @@ fn main() {
                     output = args[2][0..args[2].len() - 3].to_string();
                 }
                 Handling::Komprimer => {
-                    output = format!("{}.lz", input);
+                    output = format!("{}.lzh", input);
                 }
             }
         }
@@ -498,16 +658,25 @@ fn main() {
 
     match handling {
         Handling::Komprimer => {
-            resultat = lz77::komprimer(&data);
+            let huffman_komprimert = huffman::komprimer(&data);
+            resultat = lz77::komprimer(&huffman_komprimert);
         }
         Handling::Dekomprimer => {
-            resultat = match lz77::dekomprimer(&data) {
+            let lz_dekomprimert = match lz77::dekomprimer(&data) {
                 Ok(data) => data,
                 Err(melding) => {
                     println!("{melding}");
                     exit(1);
                 }
             };
+
+            resultat = match huffman::dekomprimer(&lz_dekomprimert) {
+                Ok(data) => data,
+                Err(melding) => {
+                    println!("{melding}");
+                    exit(1);
+                }
+            }
         }
     };
 
@@ -519,4 +688,3 @@ fn main() {
         }
     }
 }
-*/
